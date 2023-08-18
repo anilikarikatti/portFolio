3af35f4c9b2358d50b7043d56f9d344f9940f392
anilikarikatti <anilkarikatti333@gmail.com>
2023-08-18 12:05:10 +0530

    title added

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Image from 'next/image'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title> anil karikatii</title>
       <body>
         <Main />
         <NextScript />
